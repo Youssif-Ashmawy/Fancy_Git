866911ae23097f9d1c9a78188f6c523a88bc046c
omaroymk <omaroymk@gmail.com>
2026-03-06 17:19:10 +0200

    added new attributes in repostate dataclass and an example above

--- src/repo_state.py
+++ src/repo_state.py
@@ -7,16 +7,23 @@
 #  unstaged_files=["git_runner.py"],
 #  untracked_files=["test.py"],
 #  has_conflicts=True
+#  is_merging = True,
+#  conflicts = [MergeConflict, .....]etc
 # )
 
 from dataclasses import dataclass
-
+from src.merge_conflict import MergeConflict
 @dataclass (frozen = True)
 class RepoState:
     branch: str
     ahead_by: int
     behind_by: int
+
     staged_files: list[str]
     unstaged_files: list[str]
     untracked_files: list[str]
+
     has_conflicts: bool
+    is_merging: bool
+
+    conflicts:  list[MergeConflict]  # list of merge conflicts
